6f1127924f71079999d317a00d12acab0567462b
Jeremy Cline <jcline@redhat.com>
2019-10-15 22:56:30 -0400

    Switch the event implementation to use the bridge code
    While we decide whether to use the API or the database models, unify the
    code.
    
    Signed-off-by: Jeremy Cline <jcline@redhat.com>

--- patchlab/events.py
+++ patchlab/events.py
@@ -1,11 +1,12 @@
 from django.db.models.signals import post_save
-
 from patchwork.models import Patch
+from patchwork.views import utils
 
-from .models import GitForge
+from .bridge import open_merge_request
 
 
-def open_merge_request(sender, **kwargs):
+# TODO probably just hook onto the event model and look for series complete
+def patch_event_handler(sender, **kwargs):
     """
     A post-save signal handler to open a pull request whenever a patch series
     is received.
@@ -13,27 +14,19 @@ def open_merge_request(sender, **kwargs):
     Args:
         sender (Patch): The model class that was saved.
     """
-    print("Opening pull request...")
     instance = kwargs["instance"]
     created = kwargs["created"]
 
     # TODO figure out if/when patches are modified
     if not created:
         return
-
     # TODO why is the series field nullable?
     if not (instance.series and instance.series.received_all):
         return
 
-    # Apply patches to the local clone, git push using the API token we have
-    # TODO write model mapping gitlab project to a patchwork Project to store
-    # API tokens, SCM url(s), etc.
-    patches = instance.series.patches.all()
-
-    # TODO handle missing etc
-    forge = GitForge.objects.get(project=instance.project.patch_project)
-
-    # git am on forge.path, push -o pr using the token from GitForge
+    mbox = utils.series_to_mbox(instance.series)
+    branch_name = f"email/series-{instance.series.id}"
+    open_merge_request(instance.series.title, branch_name, mbox)
 
 
-post_save.connect(open_merge_request, sender=Patch, dispatch_uid="patchlab_mr")
+post_save.connect(patch_event_handler, sender=Patch, dispatch_uid="patchlab_mr")
