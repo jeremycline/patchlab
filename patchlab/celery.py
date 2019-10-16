c394ef88a33178d8ff5866de9dc549071fe03b77
Jeremy Cline <jcline@redhat.com>
2019-10-16 12:56:24 -0400

    Add celery workers to the development role
    Also fixes the celery app configuration.
    
    Signed-off-by: Jeremy Cline <jcline@redhat.com>

--- patchlab/celery.py
+++ patchlab/celery.py
@@ -5,6 +5,6 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patchlab.settings")
 
 #: The celery application object
-app = Celery('PatchLab')
-app.config_from_object("django:conf:settings", namespace="CELERY")
+app = Celery('patchlab')
+app.config_from_object("django.conf.settings", namespace="CELERY")
 app.autodiscover_tasks()
