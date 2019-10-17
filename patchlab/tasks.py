3a1e8843b4b3575875a0eff2fc56f040b7b976b2
Jeremy Cline <jcline@redhat.com>
2019-10-17 16:04:36 -0400

    Add prototype for the Gitlab->email bridge
    Signed-off-by: Jeremy Cline <jcline@redhat.com>

--- patchlab/tasks.py
+++ patchlab/tasks.py
@@ -1,9 +1,86 @@
+from email.policy import EmailPolicy
+from email.message import EmailMessage
+from tempfile import TemporaryDirectory
+import os
+import subprocess
+
 from celery import shared_task
+import gitlab as gitlab_module
+
+subject_prefix = "ARK INTERNAL PATCH"
+project_list = "some_list@example.com"
+project_url = "https://localhost:8443"
+
+# The policy used when parsing patches as emails.
+#
+# Refer to https://docs.python.org/3/library/email.policy.html for details on
+# individual policy options.
+email_policy = EmailPolicy()
 
 
 @shared_task
-def email_merge_request(merge_request):
-    pass
+def email_merge_request(project_id: int, merge_id: int) -> None:
+    """
+    Celery task to email a merge request.
+
+    If a merge request is made up of more than a single commit, a cover letter
+    is created using the merge request description.
+
+    Args:
+        merge_request: The web hook payload from GitLab
+    """
+    gitlab = gitlab_module.Gitlab.from_config("test")
+    project = gitlab.projects.get(project_id)
+    merge_request = project.mergerequests.get(merge_id)
+    mr_author = gitlab.users.get(merge_request.author["id"])
+    mr_from = f"{mr_author.name} <{mr_author.email}>"
+
+    # Don't spam people with merge requests that can't be merged.
+    if merge_request.merge_status != "can_be_merged":
+        return
+
+    commits = list(merge_request.commits())
+
+    cover_letter = None
+    if len(commits) > 1:
+        # Compose a cover letter based on the pull request description.
+        # TODO People may want a series summary (commits by author, etc)
+        cover_letter = EmailMessage(policy=email_policy)
+        cover_letter[
+            "Subject"
+        ] = f"[{subject_prefix} 0/{len(commits)}] {merge_request.title}"
+        cover_letter["From"] = mr_from
+        cover_letter.set_content(merge_request.description)
+
+    patches = []
+    for commit in commits:
+        # TODO collect repo root from project model
+        #
+        # This currently only works for public projects; authenticating with a
+        # token does not work.
+        # https://gitlab.com/gitlab-org/gitlab/issues/26228
+        response = gitlab.session.get(f"{project_url}/root/kernel/{commit.id}.patch")
+        response.raise_for_status()
+        patches.append(response.text)
+
+    with TemporaryDirectory() as dirname:
+        if cover_letter:
+            with open(os.path.join(dirname, "0000-cover-letter.patch"), "w") as fd:
+                fd.write(cover_letter.as_string())
+
+        for i, patch in enumerate(patches, 1):
+            with open(os.path.join(dirname, f"{str(i).zfill(4)}-.patch"), "w") as fd:
+                fd.write(patch.as_string())
+
+        subprocess.run(
+            [
+                "git",
+                "send-email",
+                f"--to={project_list}",
+                f'--subject-prefix="{subject_prefix}"',
+                dirname,
+            ]
+        )
 
 
 @shared_task
--- patchlab/views/gitlab.py
+++ patchlab/views/gitlab.py
@@ -1,16 +1,53 @@
-"""
-Web hooks for bridging GitLab into email.
-"""
-from django.http import HttpResponse
+"""Web hooks for bridging GitLab into email."""
+import json
+
+from django import http
+from django.conf import settings
+from django.views.decorators import csrf, http as http_decorators
 
 from patchlab.tasks import email_comment, email_merge_request
 
 
-def merge_request(request):
-    email_merge_request.apply_async((request,))
-    return HttpResponse()
+# TODO maybe have one URL and check X-Gitlab-Event header.
+@http_decorators.require_POST
+@csrf.csrf_exempt
+def merge_request(request: http.HttpRequest) -> http.HttpResponse:
+    """
+    Dispatch a series of emails for a merge request on Gitlab.
+
+    The request body is a JSON document documented at
+    https://docs.gitlab.com/ce/user/project/integrations/webhooks.html
+    """
+    try:
+        secret = request.headers["X-Gitlab-Token"]
+    except KeyError:
+        return http.HttpResponseForbidden("Permission denied: missing web hook token")
+
+    if secret != settings.GITLAB_WEBHOOK_SECRET:
+        return http.HttpResponseForbidden("Permission denied: invalid web hook token")
+
+    try:
+        payload = json.loads(request.body, encoding=request.encoding)
+    except json.JSONDecodeError:
+        return http.HttpResponseBadRequest("JSON body required in POST")
+
+    try:
+        if any([label["title"] == "From email" for label in payload["labels"]]):
+            return http.HttpResponse("Let's not loop infinitely")
+    except KeyError:
+        return http.HttpResponseBadRequest("Payload expected to have labels")
+
+    email_merge_request.apply_async((payload["project"]["id"], payload["object_attributes"]["id"]))
+    return http.HttpResponse()
 
 
 def comment(request):
+    """
+    Dispatch a comment posted on a pull request as an email in response to the
+    original patch series email thread.
+
+    The request body is a JSON document documented at
+    https://docs.gitlab.com/ce/user/project/integrations/webhooks.html
+    """
     email_comment.apply_async((request,))
-    return HttpResponse()
+    return http.HttpResponse()
