9e3e6369846358b653374a18192033b8d1703916
Jeremy Cline <jcline@redhat.com>
2019-10-17 16:50:33 -0400

    Add Django application boilerplate
    Configure the Django application. Running this as part of patchwork
    simply requires adding "patchlab" to the list of INSTALLED_APPS.
    
    Signed-off-by: Jeremy Cline <jcline@redhat.com>

---
+++ patchlab/apps.py
@@ -0,0 +1,19 @@
+from django.apps import AppConfig
+from django.urls import include, path
+
+
+class PatchlabConfig(AppConfig):
+    """
+    Django app configuration.
+
+    This class provides application configuration as well as a number of hooks
+    for application initialization. Refer to documentation at
+    https://docs.djangoproject.com/en/dev/ref/applications/ for available hooks.
+    """
+    name = "patchlab"
+
+    def ready(self):
+        from . import urls as our_urls
+        from patchwork import urls
+
+        urls.urlpatterns.append(path("patchlab/", include(our_urls.urlpatterns)))
